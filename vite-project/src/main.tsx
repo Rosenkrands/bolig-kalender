901752205fb0a3159760686bee226610f4a09680
kvv@kmd.dk <kvv@kmd.dk>
2025-05-25 14:27:16 +0200

    change default color

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -22,7 +22,7 @@ type ThemeContextType = {
   mode: Mode;
 };
 
-const defaultColor: Color = "lightPink";
+const defaultColor: Color = "green";
 
 const ThemeContext = React.createContext<ThemeContextType>({
   toggleMode: () => {},
